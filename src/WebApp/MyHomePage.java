ecacf25b79acc33b580c5897b9eda6681ae4e97c
shunay <zshunay@gmail.com>
2023-04-30 13:10:08 -0400

    homepage finished

--- src/WebApp/MyHomePage.java
+++ src/WebApp/MyHomePage.java
@@ -4,5 +4,5 @@ public class MyHomePage {
     //setting up homepage
     //new menu
     // more things for the homepage...logo
-    // add more things such as a fotter
+    // add more things such as a footer
 }
